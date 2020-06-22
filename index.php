a47d60b8358266e49df0a20c30ae419975d5cdf4
cid1895 <61287259+cid1895@users.noreply.github.com>
2020-06-22 11:00:41 -0400

    Intégration html
    éléments de base

---
+++ index.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+  <link
+    href="https://fonts.googleapis.com/css2?family=Playfair+Display+SC:ital,wght@1,900&family=Playfair+Display:ital,wght@0,900;1,700&family=Roboto:ital,wght@0,300;1,300&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <header>
+    <nav>
+      <h1><a href="#">Simsson Cire</a></h1>
+    </nav>
+    <div class="hero-banner">
+      <p class="slogan">Pour une sensation de propreté propre.</p>
+    </div>
+  </header>
+  <main>
+    <section>
+      <div class="container">
+        <h2>Sub-discombobulateur Atomique</h2>
+        <p>Le Sub-discombobulateur Atomique sert à nettoyer l'air
+          ambiant autour d'un joueur suite à une détéléportation... Pour une sensation
+          de propreté propre. Selon monsieur Cire co-fondateur de l'entreprise: "C'est un produit qui ne sert à
+          rien et qui coute cher, nous allons devenir RICHE!".</p>
+      </div>
+    </section>
+    <section>
+      <p class="citation">“Steve Jobs a vendu des ordinateurs en plastique coloré, nous ne vendons pas grand-chose de
+        couleur!”</p>
+      <span class="auteur">Madame Simsonne - co-fondatrice</span>
+    </section>
+    <section>
+      <div class="container">
+        <h2>Nos produits</h2>
+        <div class="row">
+          <div class="col-lg-4 card">
+            <div>
+              <img src="img/Sub-disco-1.jpg" alt="">
+            </div>
+            <div>
+              <h3>Version de base</h3>
+              <span>1250,99$</span>
+              <ul>
+                <li>Bonne pour deux utilisations.</li>
+                <li>Elle attire 50 % des molécule atomique 15 pied autour de l'utilisateur.</li>
+                <li>Pour plus de sécurité elle est impossible à modifier.</li>
+                <li>Elle est blanche</li>
+              </ul>
+              <button type="button" class="btn btn-primary">Réserver</button>
+            </div>
+          </div>
+
+          <div class="col-lg-4 card">
+            <div>
+              <img src="img/Sub-disco-2.jpg" alt="">
+            </div>
+            <div>
+              <h3>Version de base</h3>
+              <span>1250,99$</span>
+              <ul>
+                <li>Bonne pour deux utilisations.</li>
+                <li>Elle attire 50 % des molécule atomique 15 pied autour de l'utilisateur.</li>
+                <li>Pour plus de sécurité elle est impossible à modifier.</li>
+                <li>Elle est blanche</li>
+              </ul>
+              <button type="button" class="btn btn-primary">Réserver</button>
+            </div>
+          </div>
+
+          <div class="col-lg-4 card">
+            <div>
+              <img src="img/Sub-disco-3.jpg" alt="">
+            </div>
+            <div>
+              <h3>Version de base</h3>
+              <span>1250,99$</span>
+              <ul>
+                <li>Bonne pour deux utilisations.</li>
+                <li>Elle attire 50 % des molécule atomique 15 pied autour de l'utilisateur.</li>
+                <li>Pour plus de sécurité elle est impossible à modifier.</li>
+                <li>Elle est blanche</li>
+              </ul>
+              <button type="button" class="btn btn-primary">Réserver</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <p>Site réalisé par Cédric Marcoux - 2020</p>
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+    crossorigin="anonymous"></script>
+  <script src="js/vendor/modernizr-3.7.1.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
